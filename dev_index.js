3ae7eae3c596ed13d0dadd4bca5e883485f0b4d6
PDTayl888 <patrickdtaylor1980@gmail.com>
2019-03-17 23:53:18 -0400

    getting a better idea of how i want table to behave.

--- dev_index.js
+++ dev_index.js
@@ -1,8 +1,85 @@
 
-console.log('poop on poop on poop');
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+
+function myFunction() {
+  console.log('outerFunction');
+  const row = document.getElementById("poop");
+  console.log(row);
+  const x = row.insertCell(1);
+  x.innerHTML = "NEW SHIT";
+  console.log(x.innerHTML);
+
+  myOtherFunction();
+}
+function myOtherFunction() {
+  console.log('innerFunction');
+  const otherRow = document.getElementById("death");
+  console.log(otherRow);
+  const y = otherRow.insertCell(1);
+  y.innerHTML = "OTHER NEW SHIT";
+}
+
+var test = document.querySelector('.shite');
+console.log(test);
+test.setAttribute("class", "student1");
+
+function changeColor(newColor) {
+  const otherRow = document.getElementById("death");
+  console.log(otherRow);
+  console.log("shart shart");
+  var elem = document.getElementById("fuct");
+  elem.style.color = newColor;
+}
+
+changeColor('red');
+
+function addClassToSchedule() {
+  console.log('function is working!!!');
+  const tableName = document.getElementById('table_id');
+  console.log(tableName);
+  const row = tableName.insertRow(-1);
+
+  console.log('pooooooopppppp');
+}
+
+const sayHi = function() {
+  console.log("Hey there!");
+}
+
+sayHi();
+
+addClassToSchedule(8);
+
+function addRow(tableID) {
+  // Get a reference to the table
+  let tableRef = document.getElementById(tableID);
+
+  // Insert a row at the end of the table
+  let newRow = tableRef.insertRow(-1);
+
+  // Insert a cell in the row at index 0
+  let newCell = newRow.insertCell(0);
+
+  // Append a text node to the cell
+  let newText = document.createTextNode('New bottom row');
+  newCell.appendChild(newText);
+}
+
+// Call addRow() with the table's ID
+addRow('table_id');
+
+/* 
+function myFunction() {
+  //var row_poop = document.getElementById("poop");
+  var rowFart = document.getElementById("fart");
+  //var y = row_poop.insertCell(2);
+  var x = rowFart.insertCell(2);
+  x.innerHTML = "HELLO WORLD";
+  //y.innerHTML = "New cell";
+}
+*/
+
+
 
 
 /*
