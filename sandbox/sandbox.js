20ff8a7ebe47d368e2e86e785c74e04d183e9f12
PDTayl888 <patrickdtaylor1980@gmail.com>
2019-03-28 22:11:48 -0400

    Added buttons.

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,6 +1,6 @@
 
 //////////////////////////////////////////////////////////
-
+ 
 const studentsArray = [];
 
 const assignmentsArray = [];
@@ -8,6 +8,19 @@ const assignmentsArray = [];
 const forkDiv = document.querySelector('#forknutz');
 
 const classDiv = document.querySelector('#classContainer');
+
+function checkSubmit(e) {
+
+    if(e && e.keyCode == 13) {
+       console.log(`${e.keyCode} event`);
+       //document.forms[0].submit();
+    }
+    e.preventDefault();
+
+
+ } 
+ //checkSubmit();
+
 
 const tableCreator = {
     newElement: function(typeOfElement) {
@@ -67,6 +80,13 @@ const tableCreator = {
         return function(newInnerHtml) {
             return element.innerHTML = `${newInnerHtml}`;
         }
+    },
+
+    addInsideAfterLastChild: function(parent) {
+        console.log("addInsideAfterLastChild working");
+        return function(newChild) {
+            parent.insertAdjacentElement("beforeend", newChild);
+        }
     }
 };
 
@@ -77,12 +97,45 @@ const tableCreator = {
 function init() {
     console.log("fart shit fart ++++++++++++++++++++++++++");
     console.log("init function working");
-    document.getElementById('addClassButton').addEventListener('click', addClassToTable);
-    document.getElementById('addStudentButton').addEventListener('click', addStudentToTable);
-    document.getElementById('addAssignmentButton').addEventListener('click', addAssignmentToTable);
+    document.getElementById('addClassButton').addEventListener('click', addClassInputFormOpen);
+    document.getElementById('addButtonClass').addEventListener('click', addClassToTable);
+    document.getElementById('cancelButtonClass').addEventListener('click', cancelClassInputForm);
+
+    document.getElementById('addStudentButton').addEventListener('click', addStudentInputFormOpen);
+    document.getElementById('addButtonStudent').addEventListener('click', addStudentToTable);
+    document.getElementById('cancelButtonStudent').addEventListener('click', cancelStudentInputForm);
+
+    document.getElementById('addAssignmentButton').addEventListener('click', addAssignmentInputFormOpen);
+    document.getElementById('addButtonAssignment').addEventListener('click', addAssignmentToTable);
+    document.getElementById('cancelButtonAssignment').addEventListener('click', cancelAssignmentInputForm);
+
 }
 
+function addClassInputFormOpen() {
+console.log("addClassInputFormOpen function was invoked!!!!!!!^^^^!!!!!!!");
+}
 
+function cancelClassInputForm() {
+    console.log("cancelClassInputform was invoked!!@#");
+}
+
+function addStudentInputFormOpen() {
+    console.log("addStudentInputFormOpen function was invoked!_+_+_+__++_");
+}
+    
+function cancelStudentInputForm() {
+    console.log("cancelStudentInputform was invoked!!@#");
+}
+
+function addAssignmentInputFormOpen() {
+    console.log("addAssignmentInputFormOpen function was invoked!**********************");
+}
+    
+function cancelAssignmentInputForm() {
+    console.log("cancelAssignmentInputform was invoked!!@#@#@#");
+}
+    
+    
 
 
 
@@ -120,15 +173,28 @@ function addClassToTable() {
         tableCreator.setElementAttributeOf(classNameHeaderTag)("colspan", "100");
         tableCreator.setElementAttributeOf(classNameHeaderTag)("id", "classNameth");
         tableCreator.setElementAttributeOf(classNameHeaderTag)("style", "background-color: brown");
-        tableCreator.addTextContentTo(classNameHeaderTag)("Communications 101");
+        const classText = document.getElementById("addClassInputField").value;
+        tableCreator.addTextContentTo(classNameHeaderTag)(`${classText}`);
         tableCreator.addChildTo(firstTableHeader)(classNameHeaderTag);
 
     // CREATE ADD STUDENT BUTTON
-        const studentButtonDiv = document.getElementById("tableButtonDiv");
-        const addStudentButton = tableCreator.newElement("button");
-        tableCreator.setElementAttributeOf(addStudentButton)("id", "addStudentButton");
-        tableCreator.addTextContentTo(addStudentButton)("ADD STUDENT");
-        tableCreator.addChildTo(studentButtonDiv)(addStudentButton);
+        // const studentButtonDiv = document.getElementById("tableButtonDiv");
+        // const addStudentButton = tableCreator.newElement("button");
+        // tableCreator.setElementAttributeOf(addStudentButton)("id", "addStudentButton");
+        // tableCreator.addTextContentTo(addStudentButton)("ADD STUDENT");
+        // tableCreator.addChildTo(studentButtonDiv)(addStudentButton);
+        // console.log(addStudentButton);
+
+        //     // CREATE ADD ASSIGNMENT BUTTON
+        //     const addAssignmentButton = tableCreator.newElement("button");
+        //     tableCreator.setElementAttributeOf(addAssignmentButton)("id", "addAssignmentButton");
+        //     tableCreator.addTextContentTo(addAssignmentButton)("ADD ASSIGNMENT");
+        //     tableCreator.addChildTo(studentButtonDiv)(addAssignmentButton);
+        //     console.log(addAssignmentButton);
+    
+        // document.getElementById('addAssignmentButton').addEventListener('click', addAssignmentToTable);
+
+        // document.getElementById('addStudentButton').addEventListener('click', addStudentToTable);
 
 }
 
@@ -139,21 +205,20 @@ function addClassToTable() {
 
 
 
-
  
 function addStudentToTable() {
-    console.log("assStudent listener is working!");
-    var x = document.getElementById("addStudentInputDiv");
-    var y = document.getElementById("addStudentInputField");
-    const inputValue = document.forms[0][0].value;
-    let inputElement = document.forms[0][0];
-    let inputForm = document.forms[0];
-    console.log(inputElement);
-    console.log(inputForm);
-    console.log(inputValue);
-    console.log(x);
-    console.log(y);
-    console.log(y.value);
+    // console.log("assStudent listener is working!");
+    // var x = document.getElementById("addStudentInputDiv");
+    // var y = document.getElementById("addStudentInputField");
+    // const inputValue = document.forms[0][0].value;
+    // let inputElement = document.forms[0][0];
+    // let inputForm = document.forms[0];
+    // console.log(inputElement);
+    // console.log(inputForm);
+    // console.log(inputValue);
+    // console.log(x);
+    // console.log(y);
+    // console.log(y.value);
 
     if(assignmentsArray.length < 1 && studentsArray.length < 1) {
         console.log("first condition test");
@@ -188,17 +253,17 @@ function addStudentToTable() {
                 studentsArray.push(first);    
 
     } else if(assignmentsArray.length < 1 && studentsArray.length >= 1) {
-                // ADD th ELEMENT TO STUDENT ROW
                 console.log("second condition test");
                 const studentId = studentsArray.length + 1;
-                const secondTh = tableCreator.newElement("th");  
-                tableCreator.addChildTo(document.getElementById("studentNamesRow"))(secondTh);
-    
+            // CREATE th HEADER FOR STUDENT NAME
+                const secondTh = tableCreator.newElement("th");      
                 const assignArrayLength = assignmentsArray.length;
             // SET CLASS OF NEW th ELEMENT
                 tableCreator.setElementAttributeOf(secondTh)("class", `${studentId}`);
             // SET TEXT OF NEW th ELEMENT
                 tableCreator.addTextContentTo(secondTh)("Sally Forth");
+            // ADD th ELEMENT TO STUDENT ROW
+                tableCreator.addChildTo(document.getElementById("studentNamesRow"))(secondTh);
             // PUSH NEW STUDENT th ELEMENT TO STUDENTS ARRAY
                 const first = document.getElementsByClassName(`${studentId}`); 
                 studentsArray.push(first);    
@@ -235,13 +300,27 @@ function addStudentToTable() {
                 console.log(newInput);
                 tableCreator.setElementAttributeOf(newInput)("id", `student-${studentId}-assignment-${indexOne}`);
                 const park = document.querySelector(`#student-3-assignment-${assignArrayLength}`);
-                const parkValue = document.querySelector(`#student-1-assignment-1`);
-                console.log(park)
+                //const parkValue = document.querySelector(`#student-1-assignment-1`);
+                //console.log(park)
                 //console.log(parkValue.value);
-                    console.log(newInput);
+                console.log(newInput);
                 tableCreator.addChildTo(newCell)(newInput);
                 console.log(document.querySelector('#newInput'));
             })
+
+
+            const Id = studentsArray.length + 1;
+            // CREATE th HEADER FOR STUDENT TOTAL
+                const studentTotalHeader = tableCreator.newElement("th");  
+            // SET CLASS OF NEW th ELEMENT
+                tableCreator.setElementAttributeOf(studentTotalHeader)("id", `student-${Id}`);
+            // SET TEXT OF NEW th ELEMENT
+                tableCreator.addTextContentTo(studentTotalHeader)("1299");
+            
+            // ADD th ELEMENT TO TOTALS ROW
+                tableCreator.addChildTo(document.getElementById("studentTotalsRow"))(studentTotalHeader);
+
+
     }
 }
 
@@ -281,10 +360,11 @@ function addAssignmentToTable() {
 
 
     // CREATE tr ELEMENT
-        let assignmentId = assignmentsArray.length - 1 ;
-        const assignmentRow = tableCreator.newElement("tr");
+        let assignmentId = assignmentsArray.length + 1;
+    // ADD ASSIGNMENT ROW TO TABLE
+        console.log("add assignment row test @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        const assignmentRow = classTable.insertRow(assignmentId);
         tableCreator.setElementAttributeOf(assignmentRow)("id", "classRow");
-        tableCreator.addChildTo(classTable)(assignmentRow);
     
         let className = "comm101";
         let studentName = "Gary Indiana";
@@ -303,6 +383,7 @@ function addAssignmentToTable() {
         tableCreator.setElementAttributeOf(assignmentName)("data-studentName", `${studentName}`);
     // ADD NEW ASSIGNMENT th TO NEW ASSIGNMENT ROW
         tableCreator.addChildTo(assignmentRow)(assignmentName);
+    // ADD TEXT TO ASSIGNMENT HEADER CELL
         tableCreator.addTextContentTo(assignmentName)("COMMONLY CONFUSED WORDS QUIZ");
         var correctCell = assignmentsArray.length - 1;
         var lengthStudents = studentsArray.length;
@@ -335,33 +416,58 @@ function addAssignmentToTable() {
                 tableCreator.setElementAttributeOf(newCell)("class", "score");
 
     
-            tableCreator.addChildTo(newCell)(newInput);
-            console.log(document.querySelector(`#student-${studsLoop}-assignment-${assignArrayLength}`));
+                tableCreator.addChildTo(newCell)(newInput);
+                console.log(document.querySelector(`#student-${studsLoop}-assignment-${assignArrayLength}`));
             }
 
 
             if(assignmentsArray.length == 1) {
 
-                    // CREATE STUDENT NAME ROW
-                    const studentNameRow = tableCreator.newElement("tr");
-                    tableCreator.setElementAttributeOf(studentNameRow)("id", "studentNamesRow");
-                    tableCreator.setElementAttributeOf(studentNameRow)("style", "background-color: rgb(255, 81, 0)");
-                    tableCreator.addChildTo(classTable)(studentNameRow);
+                // CREATE STUDENT TOTAL ROW
+                    const studentTotalRow = tableCreator.newElement("tr");
+                    tableCreator.setElementAttributeOf(studentTotalRow)("id", "studentTotalsRow");
+                    tableCreator.setElementAttributeOf(studentTotalRow)("style", "background-color: rgb(255, 81, 0)");
+                // ADD ROW TO TABLE
+                    //tableCreator.addChildTo(classTable)(studentTotalRow);
+                    tableCreator.addInsideAfterLastChild(classTable)(studentTotalRow);
+
                 
                 // CREATE HEADER TAG
                     const headerTag = tableCreator.newElement("th");
-                    tableCreator.addChildTo(studentNameRow)(headerTag);
+                // ADD HEADER TAG TO ROW
+                    tableCreator.addChildTo(studentTotalRow)(headerTag);
                     tableCreator.setElementAttributeOf(headerTag)("style", "width: 20px");
                     tableCreator.addTextContentTo(headerTag)("");
                 
                 // CREATE th ELEMENT
                     const totalTh = tableCreator.newElement("th");
-                    tableCreator.addChildTo(studentNameRow)(totalTh);
-                    const studentTotal = tableCreator.addTextContentTo(totalTh)("TOTAL");
+                    tableCreator.addChildTo(studentTotalRow)(totalTh);
+                    const studentTotal = tableCreator.addTextContentTo(totalTh)("1234");
+            
+
+          
+            console.log(`${lengthStudents} + + + + + + + + + + + + `);
+            for(i=0; i < lengthStudents; i++) {
+                stopper = 1;
+                    console.log("TOTALS ROW TEST %%%$$$$%%%%$$$$%%%%$$$$");
+                    const studentId = studentsArray.length + 1;
+                // CREATE th HEADER FOR STUDENT TOTAL
+                    const studentTotalHeader = tableCreator.newElement("th");  
+                // SET CLASS OF NEW th ELEMENT
+                    tableCreator.setElementAttributeOf(studentTotalHeader)("id", `student-${studentId}`);
+                // SET TEXT OF NEW th ELEMENT
+                    tableCreator.addTextContentTo(studentTotalHeader)("1299");
+                
+                // ADD th ELEMENT TO TOTALS ROW
+                    tableCreator.addChildTo(document.getElementById("studentTotalsRow"))(studentTotalHeader);
+    
+                    const assignArrayLength = assignmentsArray.length;
+                // PUSH NEW STUDENT th ELEMENT TO STUDENTS ARRAY
+                    //const first = document.getElementsByClassName(`${studentId}`); 
+                    //studentsArray.push(first);    
 
             }
-
-            
+            }
 }
 
 
@@ -379,20 +485,6 @@ document.addEventListener('DOMContentLoaded', init);
 
 
  
-// need to find a way to turn off listener until table is created
-const addRowEventListener = document.getElementById('addScoreButton').addEventListener("click", () => {
-    console.log(document.getElementById('studentNamesRow'));
-    console.log("newStudent working first log");
-     function newStudent(parent) {
-         console.log('newStudent working second log');
-         return (element) => {
-            tableCreator.addChildTo(parent)(`${element}`);
-        }
-     };
-
-     newStudent(document.getElementById('studentNamesRow'))("th");
-});
-
 
 //////////////////////////////////////////////////////////
 
